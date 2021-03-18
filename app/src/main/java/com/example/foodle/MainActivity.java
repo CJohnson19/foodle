e52e3cbec2fd9c6626eb69d640a1c6ea81af8de7
Sonali Mucherla <sonalimucherla@Sonalis-MacBook-Pro.local>
2021-03-18 15:12:36 -0500

    bottom navigation - sonali

--- app/src/main/java/com/example/foodle/MainActivity.java
+++ app/src/main/java/com/example/foodle/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.foodle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFrag = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFrag = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFrag = new SearchFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectedFrag = new AddFragment();
+                            break;
+                        case R.id.nav_pantry:
+                            selectedFrag = new PantryFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFrag = new PreferencesFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFrag).commit();
+                    // return clicked item
+                    return true;
+                }
+            };
 }
