652bbd5dd17f0915a7e625583727a514d9a9497e
Dhruv Methi <methi004@umn.edu>
2021-03-28 11:22:43 -0700

    adding preferences branch

--- app/src/main/java/com/example/foodle/PreferencesFragment.java
+++ app/src/main/java/com/example/foodle/PreferencesFragment.java
@@ -1,9 +1,11 @@
 package com.example.foodle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,27 @@ public class PreferencesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_preferences, container, false);
+        View view = inflater.inflate(R.layout.fragment_preferences, container, false);
+
+        Button liked_recipes = (Button) view.findViewById(R.id.liked_recipes);
+        Button user_guide = (Button) view.findViewById(R.id.user_guide);
+
+        liked_recipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+
+        });
+
+        user_guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+
+        });
+
+        return view;
     }
 }
