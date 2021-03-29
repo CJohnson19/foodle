8356698fb7a635563599581e7debc5b5eedd6c47
Bhaargav Sriraman <bhaargavsriraman@gmail.com>
2021-03-28 21:41:23 -0500

    [Feat] Adding preferences

--- app/src/main/java/com/example/foodle/PreferencesFragment.java
+++ app/src/main/java/com/example/foodle/PreferencesFragment.java
@@ -1,9 +1,11 @@
 package com.example.foodle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,19 @@ public class PreferencesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_preferences, container, false);
+        View view =  inflater.inflate(R.layout.fragment_preferences, container, false);
+        Button button = view.findViewById(R.id.FAQbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFAQ(v);
+            }
+        });
+        return view;
+    }
+
+    public void getFAQ(View view){
+        Intent intent = new Intent(getActivity(), FaqActivity.class);
+        startActivity(intent);
     }
 }
